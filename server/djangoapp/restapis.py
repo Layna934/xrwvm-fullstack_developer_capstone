90748eeb4eb20bf1ddd127c72e89ae4a0d636475
0xPrecious <113894926+Layna934@users.noreply.github.com>
2026-06-05 17:57:22 -0700

    Fix formatting and exception handling in API methods

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -19,7 +19,7 @@ def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params=params + key + "=" + value + "&"
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -35,6 +35,7 @@ def get_request(endpoint, **kwargs):
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -51,8 +52,8 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception:
         print("Network exception occurred")
