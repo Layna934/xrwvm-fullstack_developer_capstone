c2750479f283736c2971716e24541e7015d5b9e6
0xPrecious <113894926+Layna934@users.noreply.github.com>
2026-06-05 18:45:26 -0700

    Format code for better readability in views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,7 +16,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments  #  , post_review
+from .restapis import get_request, analyze_review_sentiments  # , post_review
 
 
 # Get an instance of a logger
@@ -59,7 +59,7 @@ def get_cars(request):
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "Car"
-                        "Make": car_model.car_make.name})
+                    "Make": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 # ...
@@ -92,8 +92,11 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name,
-                                            last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(username=username,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
+                                        email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -162,6 +165,6 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401,
-                                    "message": "Error in posting review"})
+                                "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
